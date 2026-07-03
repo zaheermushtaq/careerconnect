d8b0835fed1babe64198f1ad5e6e28c9a8ebaa2c
Zaheer Mushtaq <24je0691@iitism.ac.in>
2026-07-03 15:12:26 +0530

    change in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { register, login, getMe } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
-
+const authController = require("../controllers/authController");
 // Public routes (no token needed)
 router.post("/register", register);
 router.post("/login", login);
